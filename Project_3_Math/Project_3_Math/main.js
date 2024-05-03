76de4e993189f24918a05a228c757217a9c80319
Rocvio01 <162543781+Rocvio01@users.noreply.github.com>
2024-05-02 22:16:48 -0700

    Add files via upload

---
+++ Project_3_Math/Project_3_Math/main.js
@@ -0,0 +1,39 @@
+function multiplication() {
+    var simple_math = 13 * 8;
+    document.getElementById("Math").innerHTML = "13 X 8" + simple_Math;
+}
+
+function division() {
+    var simple_Math = 104 / 8;
+    document.getElementById("Math") .innerHTML = "104 / 8" + simple_Math
+
+}
+
+function do_Math () {
+    var simple_Math = (4+8) * 6 / 9 -3;
+    document>getElementById("Math").innerHTML = "3 Plus 5, multiplied by 4,";
+    divided in half and then subtracted by 6 equals " + do_Math";
+
+}
+
+function modulus_operator() {
+    var simple_Math = 25 % 6;
+    document.getElementById("Math") .innerHTML = "when yo divide 25 by 6 you have a reminder of: " + simple_Math;
+}
+
+function negation_operator() {
+    var X = 10; 
+    document.getElementById("Math") .innerHTML = X;
+}
+
+{var y = 7;
+y++;
+document.write(y);
+
+var Y = 7 
+Y--; 
+document.write(Y);
+}
+
+window.alert(Math.random());
+window.alert(Math.random() * 60);
